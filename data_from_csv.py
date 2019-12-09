cb415731b5c2d78dc9b3ddc373743be8131f756d
abey-125 <49479594+abey-125@users.noreply.github.com>
2019-12-09 21:46:31 +0530

    error fix for part 2
    data plot issue resolved

--- data_from_csv.py
+++ data_from_csv.py
@@ -5,34 +5,47 @@ Created on Sun Dec  8 13:21:05 2019
 @author: abeya
 """
 
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def avg_data_csv(year):
     temp=0
     average=[]
-    lisdata
     for row in pd.read_csv('data/AQI/aqi{}.csv'.format(year),chunksize=24):
         add_var=0
         avg=0.0
         data=[]
-        df=pd.dataframe(data=row)
+        df=pd.DataFrame(data=row)
         for index,row in df.iterrows():
             data.append(row['PM2.5'])
         for i in data :
             if type(i) is float or type(i) is int:
                 add_var=add_var+i
             elif type(i) is str:
-                if(i!='nodata' and i!='pwrfail' and i!='---' and i!='Invld'):
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='InVld':
                     temp=float(i)
                     add_var=add_var+temp
         avg=add_var/24
-        temp_i=temp_i+1
+        #temp_i=temp_i+1
         average.append(avg)
-        return average
-if __name__=='___main__':
+    return average
+if __name__=="__main__":
+
+    print("started")
     lstdata=[]
-    for year in range(2013,2018):
-        lstdata[i]=avg_data_csv(year)
-        plt.plot(range(0,360),lst2013,label='{} data'.format(year))
-        plt.show
+    i=0
+    #plt.plot(range(0,365),lst2013,label="2013 data")
+    for year in range(2013,2019):
+        lst_data=avg_data_csv(year)
+        lstdata.append(lst_data)
+        #plt.plot(range(0,365),lstdata[i],label='{}data '.format(year))
+        if(year==2014) or year==2018:
+            plt.plot(range(0,364),lstdata[i],label='{}data '.format(year))
+        else:
+            plt.plot(range(0,365),lstdata[i],label='{}data '.format(year))
+        i +=1
+    plt.xlabel('Day')
+    plt.ylabel('PM 2.5')
+    plt.legend(loc='upper right')
+    print("sucess")
+    plt.show
--- page_download.py
+++ page_download.py
@@ -10,7 +10,7 @@ import requests
 import sys
 
 def retrive_hmtl():
-    for year in range(2013,2018):
+    for year in range(2013,2019):
         for month in range(1,13):
             if(month<10):
                 url='https://en.tutiempo.net/climate/0{}-{}/ws-432950.html'.format(month,year)
@@ -19,12 +19,12 @@ def retrive_hmtl():
             texts=requests.get(url)
             text_utf=texts.text.encode('utf=8')
             
-            if not os.path.exists('/data/html_data/{}'.format(year)):
-                os.makedirs('/data/html_data/{}'.format(year))
-            with open('/data/html_data/{}/{}.html'.format(year,month),"wb") as output:
+            if not os.path.exists('data/html_data/{}'.format(year)):
+                os.makedirs('data/html_data/{}'.format(year))
+            with open('data/html_data/{}/{}.html'.format(year,month),"wb") as output:
                 output.write(text_utf)
                 
-            sys.stdout.flush()
+        sys.stdout.flush()
                 
                 
 if __name__=="__main__":
